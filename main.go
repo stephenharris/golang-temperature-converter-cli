61b83f90240b3efba59fe29c23e4ef0a79451f5d
Stephen Harris <contact@stephenharris.info>
2020-12-20 12:17:15 +0000

    module 2

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+const (
+	CELSIUS    = "C"
+	FAHRENHEIT = "F"
+)
+
 var originUnit string
 var originValue float64
 
@@ -27,9 +32,27 @@ func main() {
 	for {
 		fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
+		_, err := fmt.Scanln(&originValue)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if originUnit == CELSIUS {
+			convertToFahrenheit(originValue)
+		} else {
+			convertToCelsius(originValue)
+		}
+
 		fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
-		if shouldConvertAgain != "Y" {
+		_, err = fmt.Scanln(&shouldConvertAgain)
+
+		if err != nil {
+			printError(errReadingInput)
+		}
+
+		if strings.TrimSpace(strings.ToUpper(shouldConvertAgain)) != "Y" {
 			fmt.Println("Good bye!")
 			break
 		}
